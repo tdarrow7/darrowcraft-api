9e6b6cb21fe090a306a94cb09bc09909308d0550
tdarrow7 <tdarrow7@users.noreply.github.com>
2024-06-27 17:05:54 -0700

    chore/completed conversion of viewsets into model viewsets

--- apiapp/views.py
+++ apiapp/views.py
@@ -1,43 +1,20 @@
 # example/views.py
-from rest_framework import generics, viewsets
+from rest_framework import viewsets
 from rest_framework.response import Response
 
 from .serializers import CoffeeSerializer, BrandSerializer, RoastTypeSerializer
 from .models import RoastType, Brand, Coffee
-from django.shortcuts import get_object_or_404, get_list_or_404
 
-class BrandViewSet(viewsets.ViewSet):
-    def list(self, request):
-            queryset = Brand.objects.all()
-            serializer = BrandSerializer(queryset, many=True)
-            return Response(serializer.data)
+class BrandViewSet(viewsets.ModelViewSet):
+    queryset=Brand.objects.all()
+    serializer_class= BrandSerializer
     
-    def retrieve(self, request, pk=None):
-            queryset = Brand.objects.all()
-            brand = get_object_or_404(queryset, pk=pk)
-            serializer = BrandSerializer(brand)
-            return Response(serializer.data)
+class RoastTypeViewSet(viewsets.ModelViewSet):
+    queryset=RoastType.objects.all()
+    serializer_class=RoastTypeSerializer
+
+
     
-class RoastTypeViewSet(viewsets.ViewSet):
-    def list(self, request):
-            queryset = RoastType.objects.all()
-            serializer = RoastTypeSerializer(queryset, many=True)
-            return Response(serializer.data)
-    
-    def retrieve(self, request, pk=None):
-            queryset = RoastType.objects.all()
-            roastType = get_object_or_404(queryset, pk=pk)
-            serializer = RoastTypeSerializer(roastType)
-            return Response(serializer.data)
-    
-class CoffeeViewSet(viewsets.ViewSet):
-    def list(self, request):
-            queryset = Coffee.objects.all()
-            serializer = CoffeeSerializer(queryset, many=True)
-            return Response(serializer.data)
-    
-    def retrieve(self, request, pk=None):
-            queryset = Coffee.objects.all()
-            coffee = get_object_or_404(queryset, pk=pk)
-            serializer = CoffeeSerializer(coffee)
-            return Response(serializer.data)
+class CoffeeViewSet(viewsets.ModelViewSet):
+    queryset=Coffee.objects.all()
+    serializer_class=CoffeeSerializer
